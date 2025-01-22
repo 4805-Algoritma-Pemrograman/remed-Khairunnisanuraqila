ec820618eea9bd6e2c89826d2e84dd6467b67847
Khairunnisanuraqila <khairunnisanuraqila121@gmail.com>
2025-01-22 14:13:37 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
 public class Nomor1{
+     
      public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+       System.out.println("masukan total kilometer (km):");
+       int totalKm = input.nextInt();
+
+       System.out.println("masukan total jam lembur:");
+       int jamLembur = input.nextInt();
+
+
+       int biayaTaksi;
+
+       if (jamLembur >3) {
+        biayaTaksi = 20000 + ((totalKm - 3) > 0 ? (totalKm - 3) * 10000 : 0);
+       } else {
+        biayaTaksi = 20000;
+       }
+       System.err.println("total km: + totalKm");
+       System.err.println("kehormatan taksi: " + biayaTaksi + "0");
     }
 }
